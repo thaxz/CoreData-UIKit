24ba595d3a0542942cd6b35ae2f78bc0e3471625
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-09-08 16:43:51 -0300

    feature(CoreData): preseting action sheet to edit items

--- CoreDataUIKit/ViewController+TableView.swift
+++ CoreDataUIKit/ViewController+TableView.swift
@@ -21,9 +21,35 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let item = models[indexPath.row]
+        addSheet(item: item)
+    }
     
-    
-    
+    func addSheet(item: ToDoListItem){
+        let sheet = UIAlertController(title: "Edit", message: nil, preferredStyle: .actionSheet)
+        // all available actions
+        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        // edit action shows an alert with a textfield in it
+        sheet.addAction(UIAlertAction(title: "Edit", style: .default, handler: { [weak self] _ in
+            let alert = UIAlertController(title: "Edit Item", message: "Edit your item", preferredStyle: .alert)
+            alert.addTextField()
+            alert.textFields?.first?.text = item.name
+            alert.addAction(UIAlertAction(title: "Save", style: .cancel, handler: {_ in
+                guard let field = alert.textFields?.first, let newName = field.text, !newName.isEmpty else { return}
+                self?.updateItem(item: item, newName: newName)
+            }))
+            self?.present(alert, animated: true)
+        }))
+        
+        
+        sheet.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { [weak self] _ in
+            self?.deleteItem(item: item)
+        }))
+        present(sheet, animated: true)
+    }
     
     
 }
