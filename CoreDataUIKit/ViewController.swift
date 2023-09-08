60ce1104cc8109f18ab14047fd3e0b4918a74804
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-09-08 15:42:47 -0300

    first commit

---
+++ CoreDataUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataUIKit
+//
+//  Created by thaxz on 08/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
